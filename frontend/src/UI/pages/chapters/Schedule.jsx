fcc67d3bb45d9bacfff84f94d73cdcbd394c3a16
Vladdeek <vladryazanov2709@gmail.com>
2025-05-30 11:05:56 +0300

    dynamic schedule

--- frontend/src/UI/pages/chapters/Schedule.jsx
+++ frontend/src/UI/pages/chapters/Schedule.jsx
@@ -6,8 +6,80 @@ import {
 	ScheduleCard,
 	TeacherScheduleCard,
 } from '../../components/ScheduleCard'
+import { parse, isWithinInterval } from 'date-fns'
 
 const Schedule = ({ Month, Today, userRoles }) => {
+	const lessonTimes = [
+		'8:00 - 9:20',
+		'9:30 - 10:50',
+		'11:00 - 12:20',
+		'12:40 - 14:00',
+		'14:10 - 15:30',
+		'15:40 - 17:00',
+		'17:10 - 18:30',
+		'18:40 - 20:00',
+	]
+
+	const lessons = [
+		{
+			lessonTime: 1,
+			LessonName: 'Технологии WEB-Разработки',
+			Teacher: 'Букреев Д. А.',
+			lessonType: 'Лекция',
+			Auditoria: '7.203',
+			role: 'Студент',
+		},
+		{
+			lessonTime: 3,
+			LessonName: 'Система управления базами данных PostgreSQL',
+			Teacher: 'Ступницкий В. С.',
+			lessonType: 'Лекция',
+			Auditoria: '7.203',
+			role: 'Студент',
+		},
+		{
+			lessonTime: 6,
+			LessonName: 'Анализ данных на языке Python',
+			Teacher: 'Мозговенко А. А.',
+			Auditoria: '7.308',
+			sub_group: 1,
+			role: 'Студент',
+		},
+		{
+			lessonTime: 6,
+			LessonName: 'Технологии WEB-Разработки',
+			Teacher: 'Букреев Д. А.',
+			Auditoria: '7.308',
+			sub_group: 2,
+			role: 'Студент',
+		},
+		{
+			lessonTime: 1,
+			LessonName: 'Технологии WEB-Разработки',
+			Group: ['31 ИТ', '32 ИТ', '33 ИТ'],
+			Auditoria: '7.308',
+			lessonType: '',
+			role: 'Преподаватель',
+		},
+		{
+			lessonTime: 2,
+			LessonName: 'Технологии WEB-Разработки',
+			Group: ['31 ИТ', '32 ИТ'],
+			Auditoria: '7.203',
+			lessonType: 'Лекция',
+			role: 'Преподаватель',
+		},
+		{
+			lessonTime: 4,
+			LessonName: 'Технологии WEB-Разработки',
+			Group: ['31 ИТ'],
+			Auditoria: '7.203',
+			lessonType: 'Лекция',
+			sub_group: 2,
+			role: 'Преподаватель',
+		},
+	]
+
 	const hasRole = requiredRoles => {
 		return requiredRoles.some(role => userRoles.includes(role))
 	}
@@ -15,6 +87,18 @@ const Schedule = ({ Month, Today, userRoles }) => {
 
 	const handleToggle = newRole => {
 		setRole(newRole)
+	}
+
+	const getCurrentLessonStatus = timeRange => {
+		if (!timeRange) return false
+
+		const [startStr, endStr] = timeRange.split(' - ')
+		const now = new Date()
+
+		const start = parse(startStr, 'HH:mm', now)
+		const end = parse(endStr, 'HH:mm', now)
+
+		return isWithinInterval(now, { start, end })
 	}
 	return (
 		<>
@@ -32,109 +116,49 @@ const Schedule = ({ Month, Today, userRoles }) => {
 								</div>
 							)}
 
-							{role === 'Студент' ? (
-								<>
-									<ScheduleTable LessonTime={'8:00 - 9:20'}>
-										<ScheduleCard
-											LessonName={'Технологии WEB-Разработки'}
-											LessonTime={'8:00 - 9:20'}
-											Teacher={'Букреев Д. А.'}
-											lessonType={'Лекция'}
-											Auditoria={'7.308'}
-										/>
-									</ScheduleTable>
-									<ScheduleTable LessonTime={'9:30 - 10:50'}>
-										<EmptyScheduleTable />
-									</ScheduleTable>
+							{Array.from({ length: 8 }).map((_, index) => {
+								const lessonTimeNum = index
+								const timeLabel = lessonTimes[lessonTimeNum]
 
-									<ScheduleTable LessonTime={'11:00 - 12:20'}>
-										<ScheduleCard
-											LessonName={'Система управления базами данных PostgreSQL'}
-											LessonTime={'11:00 - 12:20'}
-											Teacher={'Ступницкий В. С.'}
-											lessonType={'Лекция'}
-											Auditoria={'7.203'}
-											lessonNow={true}
-										/>
-									</ScheduleTable>
-									<ScheduleTable LessonTime={'12:30 - 13:50'}>
-										<EmptyScheduleTable />
-									</ScheduleTable>
-									<ScheduleTable LessonTime={'14:00 - 15:20'}>
-										<EmptyScheduleTable />
-									</ScheduleTable>
-									<ScheduleTable LessonTime={'15:30 - 16:50'}>
-										<ScheduleCard
-											LessonName={'Анализ данных на языке Python'}
-											LessonTime={'15:30 - 16:50'}
-											Teacher={'Мозговенко А. А.'}
-											Auditoria={'7.308'}
-											sub_group={1}
-										/>
-										<ScheduleCard
-											LessonName={'Технологии WEB-Разработки'}
-											LessonTime={'8:00 - 9:20'}
-											Teacher={'Букреев Д. А.'}
-											Auditoria={'7.308'}
-											sub_group={2}
-										/>
-									</ScheduleTable>
-									<ScheduleTable LessonTime={'17:00 - 18:20'}>
-										<EmptyScheduleTable />
-									</ScheduleTable>
-									<ScheduleTable LessonTime={'18:20 - 19:50'}>
-										<EmptyScheduleTable />
-									</ScheduleTable>
-								</>
-							) : (
-								<>
-									<ScheduleTable LessonTime={'8:00 - 9:20'}>
-										<TeacherScheduleCard
-											LessonName={'Технологии WEB-Разработки'}
-											LessonTime={'8:00 - 9:20'}
-											Auditoria={'7.308'}
-											lessonType={''}
-											Group={['31 ИТ', '32 ИТ', '33 ИТ']}
-											lessonNow={true}
-										/>
-									</ScheduleTable>
-									<ScheduleTable LessonTime={'9:30 - 11:00'}>
-										<TeacherScheduleCard
-											LessonName={'Технологии WEB-Разработки'}
-											LessonTime={'9:30 - 11:00'}
-											Auditoria={'7.203'}
-											lessonType={'Лекция'}
-											Group={['31 ИТ', '32 ИТ']}
-										/>
-									</ScheduleTable>
+								// фильтруем занятия по времени и роли
+								const filteredLessons = lessons.filter(
+									l => l.lessonTime - 1 === lessonTimeNum && l.role === role
+								)
 
-									<ScheduleTable LessonTime={'11:00 - 12:20'}>
-										<EmptyScheduleTable />
+								return (
+									<ScheduleTable key={lessonTimeNum} LessonTime={timeLabel}>
+										{filteredLessons.length > 0 ? (
+											filteredLessons.map((lesson, idx) =>
+												role === 'Студент' ? (
+													<ScheduleCard
+														key={idx}
+														LessonName={lesson.LessonName}
+														LessonTime={timeLabel}
+														Teacher={lesson.Teacher}
+														lessonType={lesson.lessonType}
+														Auditoria={lesson.Auditoria}
+														sub_group={lesson.sub_group}
+														lessonNow={getCurrentLessonStatus(timeLabel)}
+													/>
+												) : (
+													<TeacherScheduleCard
+														key={idx}
+														LessonName={lesson.LessonName}
+														LessonTime={timeLabel}
+														Auditoria={lesson.Auditoria}
+														lessonType={lesson.lessonType}
+														Group={lesson.Group}
+														sub_group={lesson.sub_group}
+														lessonNow={getCurrentLessonStatus(timeLabel)}
+													/>
+												)
+											)
+										) : (
+											<EmptyScheduleTable />
+										)}
 									</ScheduleTable>
-									<ScheduleTable LessonTime={'12:30 - 13:50'}>
-										<TeacherScheduleCard
-											LessonName={'Технологии WEB-Разработки'}
-											LessonTime={'12:30 - 13:50'}
-											Auditoria={'7.203'}
-											lessonType={'Лекция'}
-											Group={['31 ИТ']}
-											sub_group={2}
-										/>
-									</ScheduleTable>
-									<ScheduleTable LessonTime={'14:00 - 15:20'}>
-										<EmptyScheduleTable />
-									</ScheduleTable>
-									<ScheduleTable LessonTime={'15:30 - 16:50'}>
-										<EmptyScheduleTable />
-									</ScheduleTable>
-									<ScheduleTable LessonTime={'17:00 - 18:20'}>
-										<EmptyScheduleTable />
-									</ScheduleTable>
-									<ScheduleTable LessonTime={'18:20 - 19:50'}>
-										<EmptyScheduleTable />
-									</ScheduleTable>
-								</>
-							)}
+								)
+							})}
 						</div>
 					</div>
 				</div>
